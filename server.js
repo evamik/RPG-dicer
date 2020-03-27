7b20a2777aede70a278addb564ad9862fd2570c8
Evaldas Mikalauskas <mikalauskas.e@gmail.com>
2020-03-27 17:44:21 +0200

    d

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ const path = require('path')
 require('dotenv').config();
 
 const app = express();
-const http = require('http').createServer(app)
-const io = require('socket.io').listen(http)
-const port = process.env.PORT || 5000;
+app.set('port', (process.env.PORT || 5000))
+const server = app.listen(app.get('port'), () => {
+  console.log(`running on port: ${app.get('port')}`)
+})
+const io = require('socket.io')(server)
 
 app.use(cors());
 app.use(express.json());
@@ -26,7 +28,7 @@ app.use('api/rolls', rollsRouter);
 
 const Roll = require('./models/roll')
 
-io.sockets.on('connection', (socket) => {
+io.on('connection', (socket) => {
 
     Roll.find().sort({createdAt: -1}).limit(10).exec((err, rolls) => {
       if (err) return console.error(err);
@@ -64,8 +66,3 @@ if(process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
   });
 }
-  
-http.listen(port, () => {
-    console.log('listening on *:' + port);
-});
-
